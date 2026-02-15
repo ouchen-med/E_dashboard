2d5dcac9f89826995986b980918542613f6fe7c9
Mohamed Ouchen <ouchenmohammed963@gmail.com>
2026-02-15 21:23:34 +0000

    productSchema added

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,14 +4,35 @@ const productSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      required: true,
+      trim: true
+    },
+
+    description: {
+      type: String,
       required: true
     },
+
     price: {
       type: Number,
       required: true
     },
-    
-    
+
+    discount: {
+      type: Number,
+      default: 0   
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+
+    image: {
+      type: String,   
+      required: true
+    }
   },
   { timestamps: true }
 );
